c0ca9549c5a7c04574c986d86a6c9c453a2e8fa2
btawa <brendan.tawa@gmail.com>
2021-07-25 19:35:54 -0400

    Rev 2

---
+++ loops.py
@@ -0,0 +1,46 @@
+from discord.ext import tasks, commands
+from scraper import Scraper
+
+
+class Tasks(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.scraper = Scraper()
+
+    @tasks.loop(seconds=30.0)
+    async def usapi_loop(self, ctx):
+        self.scraper.get_usapi_count()
+        if self.scraper.lastus < self.scraper.us_api_count:
+            await ctx.channel.send(f'```US API CARD COUNT HAS CHANGED:\nLast:{self.scraper.lastus}\nCurrent: {self.scraper.us_api_countt}```')
+        self.scraper.lastus = self.scraper.us_api_count
+
+    @tasks.loop(seconds=30.0)
+    async def jpcardstxt_loop(self, ctx):
+        self.scraper.get_listcardtxt_count()
+        if self.scraper.lastjp < self.scraper.jp_listcardtxt_count:
+            await ctx.channel.send(f'```JP CARDTXT COUNT HAS CHANGED:\nLast:{self.scraper.lastjp}\nCurrent: {self.scraper.jp_listcardtxt_count}```')
+        self.scraper.lastjp = self.scraper.jp_listcardtxt_count
+
+    @tasks.loop(seconds=5)
+    async def spoof_loop(self, ctx):
+        if self.scraper.lastspoof < self.scraper.spoof_count:
+            await ctx.channel.send(f'```SPOOF COUNT HAS CHANGED:\nLast:{self.scraper.lastspoof}\nCurrent: {self.scraper.spoof_count}```')
+        self.scraper.lastspoof = self.scraper.spoof_count
+
+    @commands.command()
+    async def spoof(self, ctx):
+        self.scraper.spoof_count += 1
+
+    @commands.command()
+    async def start(self, ctx):
+        self.usapi_loop.start(ctx)
+        self.jpcardstxt_loop.start(ctx)
+        self.spoof_loop.start(ctx)
+        await ctx.channel.send('```starting loops```')
+
+    @commands.command()
+    async def stop(self, ctx):
+        self.usapi_loop.stop(ctx)
+        self.jpcardstxt_loop.stop(ctx)
+        self.spoof_loop.stop(ctx)
+        await ctx.channel.send('```stopping loops```')
--- scraper.py
+++ scraper.py
@@ -1,16 +1,18 @@
 import re
-from discord.ext import commands, tasks
 import json
 import requests
 
+
 class Scraper:
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self):
         self.jp_listcardtxt_count = self.get_listcardtxt_count()
-        self.us_api_count = self.get_usapi_count()
-        self.lastus = self.get_usapi_count()
         self.lastjp = self.get_listcardtxt_count()
 
+        self.us_api_count = self.get_usapi_count()
+        self.lastus = self.get_usapi_count()
+
+        self.spoof_count = 0
+        self.lastspoof = 0
 
     def get_listcardtxt_count(self):
         square_jp_api = "http://www.square-enix-shop.com/jp/ff-tcg/card/data/list_card.txt"
@@ -36,20 +38,4 @@ class Scraper:
 
         self.us_api_count = content['count']
         return content['count']
-
-    @tasks.loop(seconds=30.0)
-    async def usapi_loop(self, ctx):
-        self.get_usapi_count()
-        if self.lastus < self.us_api_count:
-            await ctx.channel.send(f'```US API CARD COUNT HAS CHANGED:\nLast:{self.last}\nCurrent: {self.us_api_count}```')
-            self.lastus = self.us_api_count
-
-    @tasks.loop(seconds=30.0)
-    async def jpcardstxt_loop(self, ctx):
-        self.get_listcardtxt_count()
-        if self.lastjp < self.jp_listcardtxt_count:
-            await ctx.channel.send(f'```JP CARDTXT COUNT HAS CHANGED:\nLast:{self.last}\nCurrent: {self.us_api_count}```')
-            self.lastjp = self.jp_listcardtxt_count
-
-
 
